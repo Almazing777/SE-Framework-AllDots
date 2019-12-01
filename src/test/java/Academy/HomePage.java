5db6a476e02ce4b38ccfe4938df2bceef188b7e6
almas <mendygax@gmail.com>
2019-11-30 18:14:17 -0800

    parametrization - removing the hardcoded data

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -1,5 +1,6 @@
 package Academy;
 
+import org.testng.annotations.DataProvider;
 import pageObjects.LoginPage;
 import resources.Base;
 import org.testng.annotations.Test;
@@ -9,8 +10,8 @@ import java.io.IOException;
 
 public class HomePage extends Base {
 
-    @Test
-    public void basePageNavigation() throws IOException {
+    @Test (dataProvider = "getData")
+    public void basePageNavigation(String Username, String Password ) throws IOException {
         driver = initializeDriver();
         driver.get("http://qaclickacademy.com");
 
@@ -18,8 +19,29 @@ public class HomePage extends Base {
         l.getLogin().click();
 
         LoginPage lp = new LoginPage(driver);
-        lp.getEmail().sendKeys("abc@gmail.com");
-        lp.getPassword().sendKeys("12314124");
+        lp.getEmail().sendKeys(Username);
+        lp.getPassword().sendKeys(Password);
+//        System.out.println(text);
+        lp.getLogin().click();
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+
+        Object[][] data = new Object[1][2];
+
+        //0th row
+        data[0][0] = "abc@gmail.com";
+        data[0][1] = "123456";
+//        data[0][2] = "Restricted User";
+
+        //1st row
+//        data[1][0] = "restricteduser@qw.com";
+//        data[1][1] = "45678";
+//        data[1][2] = "Non restructed user";
+
+        return data;
+
     }
 
 }
